c1e3a272129cc470e756e098d99d167be4908029
r_haase <robert.haase@mail.de>
2017-09-11 22:20:22 -0700

    [feature] adds scripts that installs all packages needed for the web app to be fully functional

--- loadInstallPack.R
+++ loadInstallPack.R
@@ -6,10 +6,10 @@
 ####################################################
 
 #----------------- load or install packages -----------------#
+if(!require(shiny)) {install.packages("shiny"); require(shiny)}
 if(!require(nnet)) {install.packages("nnet"); require(nnet)} 
 if(!require(jsonlite)) {install.packages("jsonlite"); require(jsonlite)} 
 if(!require(mime)) {install.packages("mime"); require(mime)} 
-if(!require(shiny)) {install.packages("shiny"); require(shiny)}
 if(!require(shinythemes)) {install.packages("shinythemes"); require(shinythemes)}
 if(!require(doParallel)) {install.packages("doParallel"); require(shiny)}
 if(!require(caret)) {install.packages("caret"); require(caret)}
@@ -21,3 +21,4 @@ if(!require(arules)) {install.packages("arules"); require(arules)}
 if(!require(e1071)) {install.packages("e1071"); require(e1071)}
 if(!require(quantreg)) {install.packages("quantreg"); require(quantreg)}
 if(!require(ipred)) {install.packages("ipred"); require(ipred)}
+
